2d2f92cfb5fa2f303d6f7af41669ad20abf874f8
Haihtem <haithem@virtualbox.com>
2022-02-26 09:05:24 +0100

    Première validation

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+public class HelloWorld {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for (i = 0; i < 5; i++) {
+			System.out.println("Bienvenue dans Eclipse !");
+			}
+		System.out.println("C'est la fin !");
+
+	}
+	public int add(int x, int y) {
+		return x + y;
+	}
+/*
+	public static int ajouter(int num1, int num2) {
+		return num1 + num2;
+	}*/
+	
+	
+}
